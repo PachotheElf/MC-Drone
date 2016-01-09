5a91e8397d6dd37355d957f090498a52e0a5f643
PachotheElf <xarathos.kais@gmail.com>
2016-01-09 00:13:39 -0430

    Setting up basic movements

--- drone.lua
+++ drone.lua
@@ -5,40 +5,65 @@ serial = require("serialization")
 drone = component.droneInterface
 modem = component.modem;
 
+--	Communications
 s_add = "";
 s_port = 1;
 
+-- Program states
 running = true;
 
-local function status()
-  if(drone.isConnectedToDrone()) then
-    modem.send(s_add, s_port, serial.serialize(drone.getDronePressure()))
-    local posX, posY, posZ = drone.getDronePosition()
+--	Drone area buffer
+areaBuffer = {0,0,0,0,0,0}
+areaType = 0;
 
-    position = {posX, posY, posZ}
-    modem.send(s_add, s_port, serial.serialize(posX))
-    modem.send(s_add, s_port, serial.serialize(posY))
-    modem.send(s_add, s_port, serial.serialize(posZ))
-  else
-   print("Not Connected to drone") 
-  end
+--	Functions
+local function restoreArea()
+	drone.clearArea()
+	drone.addArea(areaBuffer[0], areaBuffer[1], areaBuffer[2], areaBuffer[3], areaBuffer[4], areaBuffer[5], areaType)
+end
+local function status()
+	if(drone.isConnectedToDrone()) then
+		modem.send(s_add, s_port, serial.serialize(drone.getDronePressure()))
+		local posX, posY, posZ = drone.getDronePosition()
+
+		position = {posX, posY, posZ}
+		modem.send(s_add, s_port, serial.serialize(posX))
+		modem.send(s_add, s_port, serial.serialize(posY))
+		modem.send(s_add, s_port, serial.serialize(posZ))
+	else
+		print("Not Connected to drone") 
+	end
 end
 
 modem.open(s_port)  --  Tablet port
 while running do
-  local _,_, from, port, dist, msg = event.pull("modem_message")
-  message = serial.unserialize(msg)
-  if (message == "init") then
-    print("initializing")
-    s_add = from
-  elseif (message == "status") then
-    print("sending status")
-    status()
-  elseif (message == "shutdown") then
-    running = false;
-  else
-    print("||"..message.."||")
-  end
+	local _,_, from, port, dist, msg = event.pull("modem_message")
+	message = serial.unserialize(msg)
+	if (message == "init") then
+		print("initializing")
+		modem.send(s_add, s_port, serial.serialize("pong"))
+		s_add = from
+	elseif (message == "approachPlayer" || message == "goHome") then
+		local i = 0;
+		local homePos={0,0,0}
+		while i < 3 do
+			local _,_,_,_,_,message = event.pull("modem_message")
+			homePos[i] = serial.unserialize(message)
+			i = i + 1
+		end
+		drone.clearArea()
+		drone.addArea(homePos[0], homePos[1], homePos[2])
+		drone.setAction("goto")
+		restoreArea()
+	elseif (message == "goHome") then
+	elseif (message == "status") then
+		print("sending status")
+		status()
+		elseif (message == "shutdown") then
+		running = false;
+	else
+		print("||"..message.."||")
+	end
 end
 local actions = drone.getAllActions()
 for k,v in actions do
--- drone_interface.lua
+++ drone_interface.lua
@@ -6,6 +6,7 @@ local colors = require("colors")
 local component = require("component")
 local modem = component.modem
 local gpu = component.gpu
+local nav = component.navigation
 
 --	PROGRAM STATE
 --	0 = main screen
@@ -18,37 +19,53 @@ local running = true
 local s_port = 1;
 local s_address = "";
 
+--	PLAYER POSITION
+local POS_OFFSET {256, 0, 512}
+local playerPos = {0,0,0}
+
 --	DRONE STATUS
 local pressure = 0.0;
 local position = {0,0,0}
-local posX = 0.0;
-local posY = 0.0;
-local posZ = 0.0;
+
+--	DRONE AREAS
+local importChestPos = {0,0,0}
+local exportChestPos = {0,0,0}
+local homePos = {0,0,0}
+local workingCenter = {0,0,0}
+local workingArea = {0,0,0,0,0,0}
 
 
 --	FUNCTIONS
 function API.fillTable()
 	API.setTable("Close", shutdown, 70,80,1,1)
 	if(state == 0) then	--	Main screen
-		API.setTable("Status", getStatus, 10, 20, 3, 3)
+		API.setTable("Come here", approachPlayer, 5, 20, 3, 3)
+		API.setTable("Go Home", goHome, 25, 40, 3, 3)
+		API.setTable("Set Home", setHome, 25 ,40, 6, 6)
+		API.setTable("Status", getStatus, 5, 20, 6, 6)
 	else
 	end
 	API.screen()
 end
-
-function getClick()
-  local _, _, x, y = event.pull(1,touch)
-  if x == nil or y == nil then
-    local h, w = gpu.getResolution()
-    gpu.set(h, w, ".")
-    gpu.set(h, w, " ")
-  else 
-    API.checkxy(x,y)
-  end
+function approachPlayer()
+	getPlayerPos()
+	modem.send(s_address, s_port, serial.serialize("approachPlayer"))
+	modem.send(s_address, s_port, serial.serialize(playerPos[0]))
+	modem.send(s_address, s_port, serial.serialize(playerPos[1]))
+	modem.send(s_address, s_port, serial.serialize(playerPos[2]))
 end
-
+function goHome()
+	modem.send(s_address, s_port, serial.serialize("goHome"))
+	modem.send(s_address, s_port, serial.serialize(homePos[0]))
+	modem.send(s_address, s_port, serial.serialize(homePos[1]))
+	modem.send(s_address, s_port, serial.serialize(homePos[2]))
+end
+function setHome()
+	getPlayerPos()
+	homePos = {playerPos[0], playerPos[1]-1, playerPos[2]}
+end
 function getStatus()
-	modem.broadcast(1, serial.serialize("status"))
+	modem.send(s_address, s_port, serial.serialize("status"))
 		--	Pressure
 	local _,_,_,_,_,message = event.pull("modem_message")
 	pressure = serial.unserialize(message)
@@ -63,16 +80,32 @@ function getStatus()
 	end
 
 	print("Position: "..position[0].." | "..position[1].." | "..position[2])
-
 end
 
 function shutdown()
 	running = false
 end
 
+function getPlayerPos()
+	local pPosX, pPosY, pPosZ = nav.getPosition()
+	playerPos = {pPosX + POS_OFFSET[0], pPosY + POS_OFFSET[1], pPosY + POS_OFFSET[2]}
+end
+
+function getClick()
+  local _, _, x, y = event.pull(1,touch)
+  if x == nil or y == nil then
+    local h, w = gpu.getResolution()
+    gpu.set(h, w, ".")
+    gpu.set(h, w, " ")
+  else 
+    API.checkxy(x,y)
+  end
+end
+
 --	MAIN PROGRAM
 modem.open(1)
 modem.broadcast(1, serial.serialize("init"))
+_,_, s_address,_,_, = event.pull("modem_message")
 term.setCursorBlink(false)
 gpu.setResolution(80,25)
 API.clear()
